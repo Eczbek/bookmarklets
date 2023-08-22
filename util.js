564ec1b84573656d9d445eec1ab69a2cf5f931b3
Eczbek <eczbek.void@gmail.com>
2023-08-21 20:37:59 -0400

    Fix issues

--- util.js
+++ util.js
@@ -1,31 +1,31 @@
 javascript: (async () => {
-	window.util = {
-		round: (value, decimals = 2) => Math.round(value * (10 ** decimals)) / (10 ** decimals),
+	window.util = {};
 	
-		midpoints: (min, width, count) => [...Array(count)].map((_, y) => (min * 2 + y * width * 2 + width - 1) / 2),
+	window.util.round = (value, decimals = 2) => Math.round(value * (10 ** decimals)) / (10 ** decimals);
 	
-		zip: (arr1, arr2, callback) => arr1.map((x, y) => callback(x, arr2[y])),
+	window.util.midpoints = (min, width, count) => [...Array(count)].map((_, y) => (min * 2 + y * width * 2 + width - 1) / 2);
 	
-		relativeFrequencies: (frequencies, decimals = 2) => frequencies.map((x, y, z) => window.round(x / z.reduce((a, b) => a + b), decimals)),
+	window.util.zip = (arr1, arr2, callback) => arr1.map((x, y) => callback(x, arr2[y]));
 	
-		relativeCategoryFrequencies: (freqs, minimum, width, decimals) => {
-			const g = [];
-			const max = Math.max(...freqs);
-			while (minimum <= max) {
-				g.push(freqs.reduce((x, y) => x + (y >= minimum - 0.5 && y <= minimum + width - 0.5), 0));
-				minimum += width;
-			}
-			return window.relativeFrequencies(g, decimals);
-		},
+	window.util.relativeFrequencies = (frequencies, decimals = 2) => frequencies.map((x, y, z) => window.util.round(x / z.reduce((a, b) => a + b), decimals));
 	
-		mean: (values) => values.reduce((x, y) => x + y, 0) / values.length,
-	
-		median: (values) => values.length % 2 ? values[(values.length - 1) / 2] : (values[values.length / 2 - 1] + values[values.length / 2]) / 2,
-	
-		mode: (values) => Math.max(...values),
-	
-		frequenciesMean: (frequencies, midpoints, decimals) => window.zip(frequencies, midpoints, (x, y) => x * y).reduce((x, y) => x + y, 0) / frequencies.reduce((x, y) => x + y, 0),
-	
-		removeElement: (x) => x.parentElement.removeChild(x)
+	window.util.relativeCategoryFrequencies = (freqs, minimum, width, decimals) => {
+		const g = [];
+		const max = Math.max(...freqs);
+		while (minimum <= max) {
+			g.push(freqs.reduce((x, y) => x + (y >= minimum - 0.5 && y <= minimum + width - 0.5), 0));
+			minimum += width;
+		}
+		return window.util.relativeFrequencies(g, decimals);
 	};
+	
+	window.util.mean = (values) => values.reduce((x, y) => x + y, 0) / values.length;
+	
+	window.util.median = (values) => values.length % 2 ? values[(values.length - 1) / 2] : (values[values.length / 2 - 1] + values[values.length / 2]) / 2;
+	
+	window.util.mode = (values) => Math.max(...values);
+	
+	window.util.frequenciesMean = (frequencies, midpoints, decimals) => window.util.zip(frequencies, midpoints, (x, y) => x * y).reduce((x, y) => x + y, 0) / frequencies.reduce((x, y) => x + y, 0);
+	
+	window.util.removeElement = (x) => x.parentElement.removeChild(x);
 })();
