2a1e92c974c6b3e6d536d9233060642c8287ebc3
Eczbek <eczbek.void@gmail.com>
2023-08-23 10:17:31 -0400

    Update util.js

--- util.js
+++ util.js
@@ -1,14 +1,31 @@
+// for statistics homework
+
 javascript: (async () => {
 	window.util = {};
 	
 	window.util.round = (value, decimals = 2) => Math.round(value * (10 ** decimals)) / (10 ** decimals);
 	
-	window.util.midpoints = (min, width, count) => [...Array(count)].map((_, y) => (min * 2 + y * width * 2 + width - 1) / 2);
-	
 	window.util.zip = (callback, arrays) => [...Array(Math.max(...arrays.map((array) => array.length)))].fill().map((_, i) => {
 		const elements = arrays.filter((array) => array.length > i).map((array) => array[i]);
 		return elements.length < 2 ? elements[0] : elements.slice(1).reduce(callback, elements[0]);
 	});
+
+	window.util.frequencies = (values) => values.reduce((result, item) => {
+		if (!result[item]) {
+			result[item] = 0;
+		}
+		++result[item];
+		return result;
+	}, {});
+
+	window.util.categoryFrequencies = (values, min, width) => {
+		const g = [];
+		for (let i = 0; min <= Math.max(...values); ++i) {
+			g[i] = values.reduce((x, y) => x + (y >= min && y < min + width), 0);
+			min += width;
+		}
+		return g;
+	};
 	
 	window.util.relativeFrequencies = (frequencies) => frequencies.map((x, y, z) => x / z.reduce((a, b) => a + b, 0));
 	
@@ -16,11 +33,13 @@ javascript: (async () => {
 		const g = [];
 		const max = Math.max(...freqs);
 		while (minimum <= max) {
-			g.push(freqs.reduce((x, y) => x + (y >= minimum - 0.5 && y <= minimum + width - 0.5), 0));
+			g.push(freqs.reduce((x, y) => x + (y >= minimum && y <= minimum + width), 0));
 			minimum += width;
 		}
 		return window.util.relativeFrequencies(g);
 	};
+
+	window.util.categoryMidpoints = (min, width, count) => [...Array(count)].map((_, y) => (min * 2 + y * width * 2 + width - 1) / 2);
 	
 	window.util.mean = (values) => values.reduce((x, y) => x + y, 0) / values.length;
 	
@@ -31,4 +50,6 @@ javascript: (async () => {
 	window.util.frequenciesMean = (frequencies, midpoints) => window.util.zip(frequencies, midpoints, (x, y) => x * y).reduce((x, y) => x + y, 0) / frequencies.reduce((x, y) => x + y, 0);
 	
 	window.util.removeElement = (x) => x.parentElement.removeChild(x);
+
+	window.util.getData = () => [...document.querySelectorAll('div>span>div>div>.AnsedObject')].map((x) => +x.textContent);
 })();
