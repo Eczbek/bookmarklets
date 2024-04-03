95bfb5c2c529d87d1f7f445a46ea54c78dd77424
Eczbek <eczbek.void@gmail.com>
2024-04-03 10:38:14 -0400

    Update edit.js

--- edit.js
+++ edit.js
@@ -1,3 +1,4 @@
 javascript: (async () => {
-	document.body.contentEditable = document.body.contentEditable === "true" ? false : true;
+	document.body.contentEditable = (document.body.contentEditable === "true") ? "false" : "true";
+	document.designMode = (document.designMode === "on") ? "off" : "on";
 })();
